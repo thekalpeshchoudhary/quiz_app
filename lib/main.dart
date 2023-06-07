e84717111956f8a3c6a534e10c909f7b6f9d27c2
Kalpesh Choudhary <kalpesh@retire100.live>
2023-06-07 23:37:02 +0530

    screen widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.deepPurple,
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [
-                  Color.fromARGB(255, 119, 0, 255),
-                  Color.fromARGB(255, 60, 0, 163),
-                ]),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz()
   );
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.deepPurple,
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Color.fromARGB(255, 119, 0, 255),
+                  Color.fromARGB(255, 60, 0, 163),
+                ]),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
